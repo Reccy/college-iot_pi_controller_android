fc5ae6e9a9354d166f84f356341494760ab21f8a
Aaron Meaney <aaronmeaney@hotmail.com>
2018-02-12 17:06:11 +0000

    Added AWS Iot Mobile Hub to project.

--- app/src/main/java/aaronmeaney/ie/iotpicontrollerandroid/StartActivity.java
+++ app/src/main/java/aaronmeaney/ie/iotpicontrollerandroid/StartActivity.java
@@ -2,6 +2,7 @@ package aaronmeaney.ie.iotpicontrollerandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.amazonaws.mobile.client.AWSMobileClient;
 
 public class StartActivity extends Activity {
 
@@ -9,5 +10,8 @@ public class StartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        // Get AWS Mobile Client instance
+        AWSMobileClient.getInstance().initialize(this).execute();
     }
 }
